b2023b736446abe15e02fa45ba834253bad64647
tahaltuntas <93127440+tahaltuntas@users.noreply.github.com>
2021-12-22 01:44:18 +0300

    Add project files.

---
+++ ZarOyunu/TavlaZari.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZarOyunu
+{
+    class TavlaZari
+    {
+        /// <summary>
+        /// Zarın alabileceği maksimum değer değişkeni
+        /// </summary>
+        int maksimumZarDegeri;
+
+        Random random = new Random();
+
+        /// <summary>
+        /// Zarın labileceği maksimum değeri giriniz
+        /// </summary>
+        /// <param name="maksimumZarDegeri"></param>
+
+        public TavlaZari(int maksimumZarDegeri)
+        {
+          this.maksimumZarDegeri = maksimumZarDegeri;
+        }
+        
+        /// <summary>
+        /// Rastgele bir sayı için zar at
+        /// </summary>
+        /// <returns></returns>
+
+        public int ZarAt()
+        {
+            //Rastgele sayı üretilecek
+            return random.Next(1, maksimumZarDegeri + 1);
+
+        }      
+    }
+}
